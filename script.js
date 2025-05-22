c91746a19b4a6b27423ff806cba9e80214037b3f
raksha-rajpoot <rakraj654@gmail.com>
2025-05-22 18:02:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 // complete this js code
 function Person(name, age) {
-	 this.name = name;
+  this.name = name;
   this.age = age;
 }
 
 Person.prototype.greet = function () {
-  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+  // Fix: Use "and" instead of a comma
+  console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
 };
 
 function Employee(name, age, jobTitle) {
-	 Person.call(this, name, age);
+  Person.call(this, name, age);
   this.jobTitle = jobTitle;
 }
 
 Employee.prototype = Object.create(Person.prototype);
 Employee.prototype.constructor = Employee;
+
 Employee.prototype.jobGreet = function () {
+  // This format is already correct according to your test
   console.log(
     `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
   );
@@ -24,4 +27,5 @@ Employee.prototype.jobGreet = function () {
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
+
 
